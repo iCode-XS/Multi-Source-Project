61da2839f09e69c0847cf6b70be3763ff2d9f8c2
Shivam Kanwar <shivamkanwar701@gmail.com>
2026-07-14 21:26:48 +0530

    Multi-source-project | Version 3.11 | Script is pointing towards name and address of the dealers from the individual dealer info page

--- website_3.py
+++ website_3.py
@@ -10,19 +10,49 @@ session = pipeline.init_session(pipeline.chromium_linux)
 
 response = pipeline.fetch_website(session, target_url, timer=20)
 
-def product_link_harvester(response_object):
+
+def product_link_harvester(response_object, links_list):
 
     soup = pipeline.parse_website(response_object)
 
-    #container = soup.find('div', class_='animate')
+    # container = soup.find('div', class_='animate')
 
     links_container = soup.find_all('div', class_='content p-2 md:p-4 flex flex-col')
 
     for x in links_container:
 
         links = x.find('a')
-        links_href = links.get('href', 'N/A') 
-        print(links_href)
+        links_href = links['href']
+        links_list.append(links_href)
 
 
-parse = product_link_harvester(response)
+def extraction(links_list):
+
+    while links_list:
+
+        current_url = links_list.pop(0)
+
+        print(current_url)
+
+        response = pipeline.fetch_website(session, current_url, timer=20)
+
+        soup = pipeline.parse_website(response)
+
+        shop_container = soup.find('div', class_='column profile-title')
+
+        shop = shop_container.find('h1')
+
+        shop_name = shop.text
+
+        address_container = soup.find('div', class_='mb-6 flex flex-row')
+
+        address = soup.find('span', class_='italic text-sand-darker text-sm md:text-base leading-loose').text
+
+        break
+
+
+all_websites = []
+
+harvest = product_link_harvester(response, all_websites)
+
+extract = extraction(all_websites)
