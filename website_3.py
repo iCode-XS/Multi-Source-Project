4d5c2ada90e0d3578c829056f1e243d15bb9c1fc
Shivam Kanwar <shivamkanwar701@gmail.com>
2026-07-14 20:40:00 +0530

    Multi-source-project | Version 3.10 | Links of the dealer's info page - can be extracted by the script from the all dealers page

--- website_3.py
+++ website_3.py
@@ -4,10 +4,25 @@ from core import pipeline
 
 base_url = 'https://www.pbfa.org'
 
-target_url = 'https://www.pbfa.org/members'
+target_url = 'https://www.pbfa.org/shops'
 
 session = pipeline.init_session(pipeline.chromium_linux)
 
 response = pipeline.fetch_website(session, target_url, timer=20)
 
-soup = pipeline.parse_website(response)
+def product_link_harvester(response_object):
+
+    soup = pipeline.parse_website(response_object)
+
+    #container = soup.find('div', class_='animate')
+
+    links_container = soup.find_all('div', class_='content p-2 md:p-4 flex flex-col')
+
+    for x in links_container:
+
+        links = x.find('a')
+        links_href = links.get('href', 'N/A') 
+        print(links_href)
+
+
+parse = product_link_harvester(response)
