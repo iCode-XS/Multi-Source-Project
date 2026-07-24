7f7ba839cc013f78de8841e0311d014f6ee7c0f3
Shivam Kanwar <shivamkanwar701@gmail.com>
2026-07-24 23:31:28 +0530

    Multi-source-project | Version 4.15 | Script is pointing towards all the data that was remaining

--- website_4.py
+++ website_4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from core import pipeline
+import time
 
 
 target_url = 'https://www.ioba.org/members-directory'
@@ -11,10 +12,49 @@ response = pipeline.fetch_website(session, target_url, timer=20)
 
 parsed = pipeline.parse_website(response)
 
-container = parsed.find_all('div', class_='_FiCX')
+# Number pattern generator for messy address logic
+# Don't blame me, this is my current skill level! Logic might be different if I was practicing css selectors based scraping
 
-for x in container:
+addr_list = [2]
 
-    title = x.find('span', class_='wixui-rich-text__text', style='font-weight:bold;').text
+addr_num = 2
 
-    contact = x.find('span', class_='wixui-rich-text__text', style='font-size:14px;').text
+for x in range(9):
+
+    addr_num += 3
+
+    addr_list.append(addr_num)
+
+# Extraction function
+
+def extraction(bs4_object):
+
+    container = bs4_object.find_all('div', class_='_FiCX')
+
+    for x in container:
+
+        title = x.find('span', class_='wixui-rich-text__text', style='font-weight:bold;').text
+
+        contact = x.find('span', class_='wixui-rich-text__text', style='font-size:14px;').text
+
+        address = parsed.find_all('p', class_='font_8 wixui-rich-text__text', style='font-size:14px; line-height:1.6em;')
+
+        total_address = len(address) // 3
+
+        length_addr_list = len(addr_list)
+
+        if length_addr_list <= total_address:
+            address_select = address[addr_list.pop(0)]
+
+        print(title)
+
+        print(contact)
+
+        print(address_select.text)
+
+        print()
+
+        time.sleep(6)
+
+
+a = extraction(parsed)
