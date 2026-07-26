68e82bdba0e3539b60852e4df283a31fa650cf8d
Shivam Kanwar <shivamkanwar701@gmail.com>
2026-07-26 12:43:23 +0530

    Multi-source-project | Version 4.16 | Script can save data into a .json file

--- website_4.py
+++ website_4.py
@@ -2,7 +2,17 @@
 
 from core import pipeline
 import time
+import json
 
+
+# Defaults
+
+time_per_iter = 0
+current_iter = 0
+
+# Code 
+
+base_url = 'https://www.ioba.org/'
 
 target_url = 'https://www.ioba.org/members-directory'
 
@@ -27,11 +37,21 @@ for x in range(9):
 
 # Extraction function
 
-def extraction(bs4_object):
+def extraction(bs4_object, list_name):
 
     container = bs4_object.find_all('div', class_='_FiCX')
 
     for x in container:
+
+        capture = {}
+
+        global current_iter
+
+        global time_per_iter
+
+        current_iter += 1
+
+        start_time = time.perf_counter()
 
         title = x.find('span', class_='wixui-rich-text__text', style='font-weight:bold;').text
 
@@ -46,15 +66,49 @@ def extraction(bs4_object):
         if length_addr_list <= total_address:
             address_select = address[addr_list.pop(0)]
 
-        print(title)
+        print(f'Current URL: {target_url}')
 
-        print(contact)
+        print(f'Extracting item: {title}')
 
-        print(address_select.text)
+        total_items = len(container)
+
+        items_left = total_items - current_iter
+
+        eta = time_per_iter * items_left
+
+        eta_in_mins = round(eta / 60, 1)
+
+        eta_in_seconds = round(eta % 60, 2)
+
+        print(f'Estimated Time of Completion: {eta_in_mins} minutes, {eta_in_seconds} seconds')
 
         print()
 
-        time.sleep(6)
+        capture['Image Source'] = 'N/A'
+
+        capture['Bookstore'] = title
+
+        capture['Place of Residence'] = 'N/A'
+
+        capture['Books by Author'] = 'N/A'
+
+        capture['Contact Source'] = contact
+
+        capture['Address'] = address_select.text
+
+        list_name.append(capture)
+
+        time.sleep(8)
+
+        end_time = time.perf_counter()
+
+        time_per_iter = round(end_time - start_time, 2)
+
+    with open('website_4.json', 'w') as f:
+
+        json.dump(list_name, f, indent=4)
 
 
-a = extraction(parsed)
+data = []
+
+a = extraction(parsed, data)
